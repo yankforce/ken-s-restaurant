c575d1b8e22c572912d18d62ba6487269ba7ae88
Andrew Sheelan <andrewsheelan@yahoo.com>
2013-04-18 09:44:46 -0400

    first commit

---
+++ MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  KensRestaurant
+//
+//  Created by Andrew on 4/18/13.
+//  Copyright (c) 2013 restaurant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
