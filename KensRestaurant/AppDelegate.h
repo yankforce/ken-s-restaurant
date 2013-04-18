e98f1b0b4ee089433d6f71eb7be6d00a2a852a81
Andrew Sheelan <andrewsheelan@yahoo.com>
2013-04-18 08:35:06 -0400

    Initial Commit

---
+++ KensRestaurant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KensRestaurant
+//
+//  Created by Andrew on 4/18/13.
+//  Copyright (c) 2013 restaurant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
